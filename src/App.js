c697a6fa37badf26777f38f56c55fd026a8de53c
Kevin Narsh <kpnaranj@uwaterloo.ca>
2021-07-15 21:40:31 -0400

    Tutorial 1 Completed - Setup of project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-
+// Public libraries
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import React from "react";
+// Private libraries
+import Notes from "./pages/notes";
+import Create from "./pages/create";
+// Function App - REAL element
 function App() {
+  // HTML Render
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        {/* Home route */}
+        <Route exact path="/">
+          <Notes />
+        </Route>
+        {/* Create route */}
+        <Route path="/create">
+          <Create />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
-
+// Default element
 export default App;
---
+++ src/pages/create.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Create() {
+  return <div>Create Page</div>;
+}
+
+export default Create;
