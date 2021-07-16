4ff75c2329599b77d78336c7883a718c1d9dcd69
Kevin Narsh <kpnaranj@uwaterloo.ca>
2021-07-16 09:57:21 -0400

    Completed set up of material ui css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
 // Public libraries
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import React from "react";
+// Material UI Customized Style
+import { createTheme, ThemeProvider } from "@material-ui/core";
+import purple from "@material-ui/core/colors/purple";
+import green from "@material-ui/core/colors/green";
 // Private libraries
 import Notes from "./pages/notes";
 import Create from "./pages/create";
+// Global functions
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: purple[500],
+    },
+    secondary: {
+      main: green[500],
+    },
+    
+  },
+  typography: {
+      fontFamily: "Quicksand",
+      fontWeightLight: 400,
+      fontWeightRegular: 500,
+      fontWeightMedium: 400,
+      fontWeightBold: 500,
+    },
+});
 // Function App - REAL element
 function App() {
   // HTML Render
   return (
-    <Router>
-      <Switch>
-        {/* Home route */}
-        <Route exact path="/">
-          <Notes />
-        </Route>
-        {/* Create route */}
-        <Route path="/create">
-          <Create />
-        </Route>
-      </Switch>
-    </Router>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Switch>
+          {/* Home route */}
+          <Route exact path="/">
+            <Notes />
+          </Route>
+          {/* Create route */}
+          <Route path="/create">
+            <Create />
+          </Route>
+        </Switch>
+      </Router>
+    </ThemeProvider>
   );
 }
 // Default element
